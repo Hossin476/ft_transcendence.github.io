0e00ddb0f7fed48fc3a3500fa1a0a14cc2ad11a4
SMAIL KIRA <108758834+kirazizi@users.noreply.github.com>
2024-07-06 18:11:06 +0100

    start-game

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,10 @@
-
+import React from "react"
 import LayoutOne from "./Layouts/LayoutOne"
 import { BrowserRouter,Routes,Route } from "react-router-dom"
 import Dashboard from "./Pages/Dashboard"
 import ManageFriends from "./Pages/ManageFriends"
+import PingPong from './Pvpgame';
+
 function App() {
   
 
@@ -14,12 +16,12 @@ function App() {
 					<Route path="/dashboard" element={<Dashboard/>}/>
 					<Route path="/managefriends" element={<ManageFriends/>}/>
 					{/* <Route path="/example" element={} */}
+					<Route path="/PvPingpong" element={<PingPong />}/>
 					/* here u w will add ur page component that has the friends bar */
 			</Route>
 		</Routes>
     </BrowserRouter>
   )
 }
-
 
 export default App
---
+++ front-end/src/Pvpgame.jsx
@@ -0,0 +1,148 @@
+// src/PingPong.jsx
+import React from 'react';
+import { PiArrowUUpLeftBold } from 'react-icons/pi';
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { useEffect } from 'react';
+
+import './Pvpgame.css';
+import './Matchmaking.css';
+
+import mypic from '/avatar/sbzizal.jpeg';
+import vs_avatar from '/avatar/lshail.jpeg';
+import avatar from '/avatar/test2.png';
+
+
+function Header() {
+  return (
+    <header className="header">
+      <button className="back-button">
+        <PiArrowUUpLeftBold style={{ fontSize: '2rem', color: 'white' }} />
+      </button>
+      <h1>PING PONG</h1>
+    </header>
+  )
+}
+
+function Mycard() {
+  return (
+    <div className="player-card">
+      <img src={mypic} alt="Avatar" className="avatar" />
+      <div className="player-info">
+        <h2>KIRAZIZI</h2>
+        <p>LEVEL 2</p>
+      </div>
+    </div>
+  );
+}
+
+function Wait_card() {
+  return (
+    <div className="add-player-card">
+      <img src={avatar} alt="Avatar" className="anonymous" />
+      <div className="add-player-info">
+        <h2>WAITING</h2>
+        <p>. . .</p>
+      </div>
+    </div>
+  );
+}
+
+function Vsplayer_card() {
+  return (
+    <div className="player-card">
+      <img src={vs_avatar} alt="Avatar" className="avatar" />
+      <div className="player-info">
+        <h2>LAHOUCINE</h2>
+        <p>LEVEL 1</p>
+      </div>
+    </div>
+  );
+}
+
+function Matchmaking_button({onClick}) {
+
+  return (
+    <div className="matchmaking">
+      <p>MATCHMAKING ...</p>
+      <button onClick={onClick} className="cancel-button">CANCEL</button>
+    </div>
+  );
+}
+
+function Add_card() {
+  return (
+    <div className="add-player-card">
+      <img src={avatar} alt="Avatar" className="anonymous" />
+      <p className="add-player-info">. . .</p>
+    </div>
+  );
+}
+
+function Start_button({onClick}) {
+  return (
+    <div className="start-button_div">
+      <div className="empty_start"></div>
+      <button onClick={onClick} className="start-button">START</button>
+    </div>
+  );
+}
+
+function Started_button({onClick}) {
+  return (
+    <div className="start-button_div">
+      <div className="empty_start"></div>
+      <button onClick={onClick} className="started-button">STARTED</button>
+    </div>
+  );
+
+}
+
+function PingPong() {
+
+  const [isstart, setStart] = React.useState(false);
+  const [isstarted, setStarted] = React.useState(false);
+
+
+  function startGame() {
+    setStart(true);
+  }
+
+  function stopGame() {
+    setStart(false);
+  }
+
+  useEffect(() => {
+    if (isstart) {
+      const timer = setTimeout(() => {
+        setStarted(true);
+      }, 3000); // 3 seconds
+      return () => clearTimeout(timer);
+    }
+  }, [isstart]);
+
+
+  return (
+    <div className="ping-pong-container">
+      <Header />
+      <div className="player-cards">
+        <Mycard />
+        {
+          isstarted ? (
+            <Vsplayer_card />
+          ) : (
+            isstart ? <Wait_card /> : <Add_card />
+          )
+        }
+      </div>
+        {
+          isstarted ? (
+            <Started_button />
+          ) : (
+            isstart ? <Matchmaking_button onClick={stopGame} /> : <Start_button onClick={startGame} />
+          )
+        }
+    </div>
+  );
+};
+
+export default PingPong;
