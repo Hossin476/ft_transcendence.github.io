a4ab70ecb07766f2d44fe3b891b1b02a858804e6
Hossin476 <hossaynsohail2018@gmail.com>
2024-07-08 18:29:28 +0100

    adding changes to tournament

--- front-end/src/Pages/Tournament.jsx
+++ front-end/src/Pages/Tournament.jsx
@@ -3,61 +3,61 @@ import '../tournament.css'
 
 function Tournament() {
   return (
-    <div className=' container_all max-w-7xl mx-auto'>
+    <div className=' container_all max-w-3xl mx-auto relative'>
       <h1 className='text-4xl bg-slate-800 text-center top_title'>PING PONG</h1>
       <h3 className='text-white text-md bg-slate-800 text-center down_title'>TOURNAMENT</h3>
-      <div className='grid grid-cols-5 grid-rows-5 gap-4 justify-items-center relative mt-32'>
+      <div className='grid grid-cols-5 grid-rows-5 gap-4 justify-items-center tor_cont'>
         <div className="rounded bg-white line_1"></div>
         <div className="rounded bg-white absolute line_2"></div>
-        <div className='corner_1'></div>
+        {/* <div className='corner_1'></div>
         <div className='corner_2'></div>
         <div className='corner_3'></div>
-        <div className='corner_4'></div>
-        <div className="col-start-1 row-start-1 relative p-10 rounded-lg text-center">
-          <div className="img_container bg-white w-32">
-            <img src="lshail.jpeg" alt="Avatar" className=" mx-auto rounded-full" />
+        <div className='corner_4'></div> */}
+        <div className="col-start-1 row-start-2 relative rounded-lg text-center">
+          <div className="img_container w-14 h-14 mx-auto">
+            <img src="lshail.jpeg" alt="Avatar" className="rounded-full" />
           </div>
-          <p className="text-white relative top-1 text-xl">Username 1</p>
+          <p className="text-white relative md:text-md lg:text-xl sm:text-sm">Username</p>
         </div>
-        <div className="col-start-1 row-start-3  p-10 rounded-lg text-center">
-          <div className="img_container w-32">
-            <img src="lshail.jpeg" alt="Avatar" className=" mx-auto rounded-full" />
+        <div className="col-start-1 row-start-4 rounded-lg text-center">
+          <div className="img_container w-14 h-14 mx-auto">
+            <img src="lshail.jpeg" alt="Avatar" className="rounded-full" />
           </div>
-          <p className="text-white relative top-1 text-xl">Username 2</p>
+          <p className="text-white relative top-1 md:text-md lg:text-xl sm:text-sm">Username</p>
         </div>
-        <div className="col-start-5 row-start-1 p-10  rounded-lg text-center">
-          <div className="img_container w-32">
-            <img src="lshail.jpeg" alt="Avatar" className=" mx-auto rounded-full" />
+        <div className="col-start-5 row-start-2  rounded-lg text-center">
+          <div className="img_container w-14 h-14 mx-auto">
+            <img src="lshail.jpeg" alt="Avatar" className="rounded-full" />
           </div>
-          <p className="text-white relative top-1 text-xl">Username 3</p>
+          <p className="text-white relative top-1 md:text-md lg:text-xl sm:text-sm">Username</p>
         </div>
-        <div className="col-start-5 row-start-3 p-10  rounded-lg text-center">
-          <div className="img_container w-32">
-            <img src="lshail.jpeg" alt="Avatar" className=" mx-auto rounded-full" />
+        <div className="col-start-5 row-start-4  rounded-lg text-center">
+          <div className="img_container w-14 h-14 mx-auto">
+            <img src="lshail.jpeg" alt="Avatar" className="rounded-full" />
           </div>
-          <p className="text-white relative top-1 text-xl">Username 4</p>
+          <p className="text-white relative top-1 md:text-md lg:text-xl sm:text-sm">Username</p>
         </div>
-        <div className="col-start-2  row-start-2 p-10 rounded-lg text-center mr-36">
-          <div className="img_container w-32 ">
-            <img src="lshail.jpeg" alt="Avatar" className=" mx-auto rounded-full" />
+        <div className="col-start-2 row-start-3 rounded-lg text-center">
+          <div className="img_container w-14 h-14 mx-auto">
+            <img src="lshail.jpeg" alt="Avatar" className="rounded-full" />
           </div>
-          <p className="text-white relative top-1 text-xl">Username 5</p>
+          <p className="text-white relative top-1 md:text-md lg:text-xl sm:text-sm">Username</p>
         </div>
-        <div className="col-start-4 row-start-2 p-10 rounded-lg text-center ml-36">
-          <div className="img_container w-32">
-            <img src="lshail.jpeg" alt="Avatar" className=" mx-auto rounded-full" />
+        <div className="col-start-4 row-start-3 rounded-lg text-center">
+          <div className="img_container w-14 h-14 mx-auto">
+            <img src="lshail.jpeg" alt="Avatar" className=" rounded-full" />
           </div>
-          <p className="text-white relative top-1 text-xl">Username 6</p>
+          <p className="text-white relative top-1 md:text-md lg:text-xl sm:text-sm">Username</p>
         </div>
-        <div className="winner_container col-start-3 row-start-2  p-10 rounded-lg text-center">
+        <div className="winner_container col-start-3 row-start-3 rounded-lg text-center">
           {/* <img src="winner.png" alt="winner" className='winner' /> */}
-          <div className="img_container w-32">
+          <div className="img_container w-14 h-14 mx-auto">
             <img src="lshail.jpeg" alt="Avatar" className=" mx-auto rounded-full" />
           </div>
-          <p className="text-white relative top-1 text-xl">Winner</p>
+          <p className="text-white relative top-1 md:text-md lg:text-xl sm:text-sm">Winner</p>
         </div>
-        <div className="bg-white w-28 h-1 rounded absolute line_3"></div>
-        <div className="bg-white w-28 h-1 rounded absolute line_4"></div>
+        <div className="bg-white  rounded absolute line_3"></div>
+        <div className="bg-white rounded absolute line_4"></div>
       </div>
     </div>
   );
