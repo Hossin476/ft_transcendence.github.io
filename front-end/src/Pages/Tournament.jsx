f7a6cb85cba5aec3ac56c709a1fcb69f686839f1
Hossin476 <hossaynsohail2018@gmail.com>
2024-07-08 20:21:56 +0100

    fixing the layout and the spacing

--- front-end/src/Pages/Tournament.jsx
+++ front-end/src/Pages/Tournament.jsx
@@ -3,21 +3,21 @@ import '../tournament.css'
 
 function Tournament() {
   return (
-    <div className=' container_all max-w-3xl mx-auto relative'>
-      <h1 className='text-4xl bg-slate-800 text-center top_title'>PING PONG</h1>
-      <h3 className='text-white text-md bg-slate-800 text-center down_title'>TOURNAMENT</h3>
-      <div className='grid grid-cols-5 grid-rows-5 gap-4 justify-items-center tor_cont'>
-        <div className="rounded bg-white line_1"></div>
-        <div className="rounded bg-white absolute line_2"></div>
+    <div className=' container_all flex flex-col justify-items-center mx-auto my-auto'>
+        <h1 className='text-4xl bg-slate-800 text-center top_title'>PING PONG</h1>
+        <h3 className='text-white text-md bg-slate-800 text-center down_title'>TOURNAMENT</h3>
+      <div className='grid grid-cols-5 grid-rows-5 gap-4 tor_cont my-auto sm:mx-5 lg:mx-20 md-mx-10'>
+        {/* <div className="rounded bg-white line_1"></div>
+        <div className="rounded bg-white absolute line_2"></div> */}
         {/* <div className='corner_1'></div>
         <div className='corner_2'></div>
         <div className='corner_3'></div>
         <div className='corner_4'></div> */}
-        <div className="col-start-1 row-start-2 relative rounded-lg text-center">
+        <div className=" relative rounded-lg text-center col-start-1 row-start-2">
           <div className="img_container w-14 h-14 mx-auto">
             <img src="lshail.jpeg" alt="Avatar" className="rounded-full" />
           </div>
-          <p className="text-white relative md:text-md lg:text-xl sm:text-sm">Username</p>
+          <p className="text-white relative md:text-md lg:text-xl sm:text-sm">Username1</p>
         </div>
         <div className="col-start-1 row-start-4 rounded-lg text-center">
           <div className="img_container w-14 h-14 mx-auto">
@@ -51,13 +51,13 @@ function Tournament() {
         </div>
         <div className="winner_container col-start-3 row-start-3 rounded-lg text-center">
           {/* <img src="winner.png" alt="winner" className='winner' /> */}
-          <div className="img_container w-14 h-14 mx-auto">
-            <img src="lshail.jpeg" alt="Avatar" className=" mx-auto rounded-full" />
+          <div className="img_container mx-auto w-14 h-14">
+            <img src="lshail.jpeg" alt="Avatar" className="rounded-full" />
           </div>
           <p className="text-white relative top-1 md:text-md lg:text-xl sm:text-sm">Winner</p>
         </div>
-        <div className="bg-white  rounded absolute line_3"></div>
-        <div className="bg-white rounded absolute line_4"></div>
+        {/* <div className="bg-white  rounded absolute line_3"></div>
+        <div className="bg-white rounded absolute line_4"></div> */}
       </div>
     </div>
   );
