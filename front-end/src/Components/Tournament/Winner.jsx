b5a98e401c0790f08def1da48ddcf96d9ba2c4e1
Hossin476 <hossaynsohail2018@gmail.com>
2024-07-17 21:25:12 +0100

    adding changes of yesterday

--- front-end/src/Components/Tournament/Winner.jsx
+++ front-end/src/Components/Tournament/Winner.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 function Winner({ className }) {
     return (
-        <div className={`${className} flex flex-col items-center h-full`}>
-            <div className="img_container w-3/4 h-4/6 mx-auto mt-auto">
-                <img src="lshail.jpeg" alt="Avatar" className="rounded-full mt-auto" />
+        <div className={`${className} flex flex-col items-center justify-center h-full w-full`}>
+            <div className="relative grid place-items-center w-full h-full">
+                <img 
+                    src="/winner.png" 
+                    alt="Winner Badge" 
+                    className="xsm:-18 xsm:h-18 sm:w-22 sm:h-22 md:w-32 md:h-32 lg:w-40 lg:h-40 col-start-1 row-start-1"
+                />
+                <img 
+                    src="lshail.jpeg" 
+                    alt="Avatar" 
+                    className="rounded-full xsm:w-16 xsm:h-16 sm:w-20 sm:h-20 md:w-16 md:h-16 lg:w-22 lg:h-22 xl:w-[5rem] xl:h-[5rem] col-start-1 row-start-1"
+                />
             </div>
-            <p className="text-white md:text-md lg:text-xl sm:text-sm mt-auto">Username</p>
+            <p className="text-center text-white text-sm md:text-md lg:text-xl mb-4">Username</p>
         </div>
-    )
+    );
 }
 
-export default Winner
+export default Winner;
--- front-end/src/Pages/Tournament.jsx
+++ front-end/src/Pages/Tournament.jsx
@@ -1,5 +1,5 @@
-// import React from 'react';
-import '../tournament.css'
+import React from 'react';
+import '../tournament.css';
 
 import User from '../Components/Tournament/User';
 import Winner from '../Components/Tournament/Winner';
@@ -10,23 +10,22 @@ function Tournament() {
   return (
     <div className="container_tournament bg-primaryColor w-full grid grid-rows-1 justify-items-center items-center">
       <div className="flex justify-center items-center w-11/12 h-full">
-        < Challenge />
-        <div className='border  border-forthColor lg:w-11/12 md:w-11/12  w-full text-white flex flex-col items-center h-5/6 justify-evenly bg-linkBgColor rounded-3xl'>
-          < Header />
-          <div className=" w-full mx-auto grid justify-center h-2/4 my-auto tor_cont px-10">
-            <div className="first_container w-3/5 h-full border-8 relative mx-auto">
-              <User className={"h-36 w-36 bg-linkBgColor top-[-4.5rem] left-[-4.5rem] absolute text-center  grid  place-content-center"} />
-              <User className={"h-36 w-36 bg-linkBgColor absolute bottom-[-4.5rem] left-[-4.5rem] text-center grid  place-content-center"} />
-              <User className={"h-36 w-36 bg-linkBgColor absolute top-[50%] translate-y-[-50%] right-[-4.5rem] text-center grid  place-content-center"} />
+        {/* < Challenge /> */}
+        <div className='border border-forthColor lg:w-11/12 md:w-11/12 w-full text-white flex flex-col items-center h-[70%] bg-linkBgColor rounded-3xl'>
+          <Header />
+          <div className="w-full grid items-center justify-items-center h-2/4 tor_cont px-10 my-auto">
+            <div className="first_container w-[40%] max-w-[200px] h-[90%]  border-8 relative">
+              <User className="h-16 w-16 sm:h-24 sm:w-24 md:h-28 md:w-28 lg:h-32 lg:w-32 bg-linkBgColor top-[-3.5rem] sm:top-[-4.5rem] left-[-3.5rem] sm:left-[-4.5rem] absolute text-center grid place-content-center" />
+              <User className="h-16 w-16 sm:h-24 sm:w-24 md:h-28 md:w-28 lg:h-32 lg:w-32 bg-linkBgColor absolute bottom-[-3.5rem] sm:bottom-[-4.5rem] left-[-3.5rem] sm:left-[-4.5rem] text-center grid place-content-center" />
+              <User className="h-16 w-16 sm:h-24 sm:w-24 md:h-28 md:w-28 lg:h-32 lg:w-32 bg-linkBgColor absolute top-[50%] translate-y-[-50%] right-[-3.5rem] sm:right-[-4.5rem] text-center grid place-content-center" />
             </div>
-            <div className="winner_container my-auto border-2 border-yellow-400 rounded-3xl h-2/4 w-full relative flex justify-center mx-auto">
-              <img src="/winner.png" alt="winner padge " className='absolute top-[50%] z-0 left-[50%] translate-x-[-50%] translate-y-[-50%]' />
-              <Winner className={'z-10'} />
+            <div className="winner_container border-2 border-yellow-400 rounded-3xl w-[100%] max-w-[200px] h-[90%]   relative flex">
+              <Winner className="z-10" />
             </div>
-            <div className="second_container w-3/5 h-full border-8 relative mx-auto">
-              <User className={"h-36 w-36 bg-linkBgColor absolute right-[-4.5rem] top-[-4.5rem] text-center grid  place-content-center"} />
-              <User className={"h-36 w-36 bg-linkBgColor absolute bottom-[-4.5rem] right-[-4.5rem] text-center grid  place-content-center"} />
-              <User className={"h-36 w-36 bg-linkBgColor absolute top-[50%] left-[-4.5rem] translate-y-[-50%] text-center grid  place-content-center"} />
+            <div className="second_container w-[40%] max-w-[200px] h-[90%]  border-8 relative">
+              <User className="h-16 w-16 sm:h-20 sm:w-20 md:h-28 md:w-28 lg:h-32 lg:w-32 bg-linkBgColor absolute right-[-3.5rem] sm:right-[-4.5rem] top-[-3.5rem] sm:top-[-4.5rem] text-center grid place-content-center" />
+              <User className="h-16 w-16 sm:h-24 sm:w-24 md:h-28 md:w-28 lg:h-32 lg:w-32 bg-linkBgColor absolute bottom-[-3.5rem] sm:bottom-[-4.5rem] right-[-3.5rem] sm:right-[-4.5rem] text-center grid place-content-center" />
+              <User className="h-16 w-16 sm:h-24 sm:w-24 md:h-28 md:w-28 lg:h-32 lg:w-32 bg-linkBgColor absolute top-[50%] left-[-3.5rem] sm:left-[-4.5rem] translate-y-[-50%] text-center grid place-content-center" />
             </div>
           </div>
         </div>
@@ -36,3 +35,4 @@ function Tournament() {
 }
 
 export default Tournament;
+
