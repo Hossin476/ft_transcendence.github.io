0ddbd40dc01f71b4ee1784fbea8c9ac6e8e32449
Hamza Younsi <hyounsi@e3r8p1.1337.ma>
2024-08-14 10:13:32 +0100

    hyounsi: i fix same problen in the  model:

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -5,7 +5,7 @@ from .managers import CustomUserManager
 
 
 
-class User(AbstractUser):
+class CustomUser(AbstractUser):
     username = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=254)
     date_joined = models.DateTimeField(auto_now_add=True)
@@ -16,10 +16,11 @@ class User(AbstractUser):
     wins = models.IntegerField()
     loses = models.ImageField()
     draws = models.IntegerField()
-    #Friend Field
-    friends = models.ManyToManyField("User", blank=True)
     #2FA
     key = models.CharField(max_length=255, null=True, default=None)
+    # profile image 
+    profile_image = models.ImageField(upload_to='images/')
+    cover_image = models.ImageField(upload_to='images/')
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = []
@@ -40,12 +41,12 @@ class Friend_Request(models.Model):
 
     request = models.CharField(max_length=1, choices=REQUEST_STATE, default='P')
     from_user = models.ForeignKey(
-        User,
+        CustomUser,
         related_name='from_user',
         on_delete=models.CASCADE
     )
     to_user = models.ForeignKey(
-        User,
+        CustomUser,
         related_name='to_user',
         on_delete=models.CASCADE
     )
