d0fa2c9ad2e4169b38b9edb44125c3796e058818
Hossin476 <hossaynsohail2018@gmail.com>
2024-09-09 17:16:54 +0100

    tournament invite modal finished

---
+++ Frontend/src/Components/Tournament/InviteModal.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import UserInvite from './UserInvite'
+import { MdClear } from "react-icons/md";
+import { useAuth } from '../../context/AuthContext';
+
+function InviteModal({ setInvite }) {
+    const [users, setUsers ] = useState([])
+    const { tokens } = useAuth()
+
+    function clear_invite() {
+        setInvite(false)
+    }
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('http://localhost:8000/notification/online/', {
+                headers: {
+                    "Authorization": "JWT " + tokens.access,
+                    "content-Type": "application/json"
+                }
+            })
+            const data = await response.json()
+            const new_table = [...data.online, ...data.offline]
+            setUsers(new_table)
+        }
+        fetchData();
+    }, [])
+    return (
+        <div className='w-[100%] h-[100%] blurHelp absolute top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%]'>
+            <div className={`w-[60%] inset-0 blurHelp h-[80%] absolute border-[3px] flex flex-col items-center z-10 gap-6 rounded-[20px] bg-gradient-to-r from-purple-400 via-pink-500 top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%] to-red-500 shadow-lg`}>
+                <div className='h-[4rem] absolute right-4 top-2'>
+                    <MdClear size={40} onClick={clear_invite} style={{ cursor: 'pointer' }} />
+                </div>
+                <h1 className={`font-Plaguard xsm:text-[3.5vw] lg:text-2xl text-white mt-4 `}> Invite Players to Tournament</h1>
+                <div className="w-[80%] p-4 space-y-4 overflow-y-auto h-[calc(100%-5rem)]">
+                    {
+                        users && users.map((user, index) => (
+                            <UserInvite user={user} key={index} />
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default InviteModal
--- Frontend/src/Pages/tour.jsx
+++ Frontend/src/Pages/tour.jsx
@@ -1,22 +1,20 @@
 import { Canvas } from "@react-three/fiber";
 import {
-   PerspectiveCamera,
    Stage,
    OrbitControls,
-   Environment,
-   KeyboardControls,
    Text
 } from "@react-three/drei";
+import { IoMdPersonAdd } from "react-icons/io";
 import { useLoader } from "@react-three/fiber";
-import { HemisphereLight, SphereGeometry, TextureLoader } from "three";
+import { TextureLoader } from "three";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { useGLTF } from '@react-three/drei';
-import { IoMdExit } from "react-icons/io";
 import { GiExitDoor } from "react-icons/gi";
 import { useEffect, useMemo, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import defaultImage from './notuser.png';
 import { useLocation } from "react-router-dom"
+import InviteModal from "../Components/Tournament/InviteModal";
 
 // const Model = () => {
 //   const gltf = useLoader(GLTFLoader, './golden_trophy.glb');
@@ -54,20 +52,7 @@ const MatchesRound = ({ player1, player2, position1, position2 }) => {
       <UserBox position={position2} username={player2 ? player2.username : "not yet"} image={player2 ? player2.profile_image : "./user.jpeg"} />
    </>)
 }
-const DisplayUser = () => {
-   const { tokens } = useAuth()
-   const [tournament, setTournament] = useState(null)
-   useEffect(() => {
-      const fetch_matches = async () => {
-         const response = await fetch('http://localhost:8000/tournament/1', {
-            headers: { Authorization: "JWT " + tokens.access }
-         })
-         const data = await response.json()
-         console.log("data matches : ", data)
-         setTournament(data)
-      }
-      fetch_matches()
-   }, [])
+const DisplayUser = ({ tournament }) => {
    const position = useMemo(() => [
       [4, 1.3, 0],
       [4, 2.3, 0],
@@ -88,32 +73,6 @@ const DisplayUser = () => {
    return (
       <>
          {tournament && tournament.matches && tournament.matches.map((item, key) => <MatchesRound key={key} player1={item.player1} player2={item.player2} position1={position[key * 2]} position2={position[key * 2 + 1]} />)}
-         {/* <UserBox position={[4, 1.3, 0]} username={"hamza"} boxcolor={'green'}/>
-         <UserBox position={[4, 2.3, 0]} username={"hamza"} boxcolor={'magenta'}/>
-
-
-         <UserBox position={[4, -1.3, 0]} username={"hamza"} boxcolor={'yellow'}/>
-         <UserBox position={[4, -2.3, 0]} username={"hamza"} boxcolor={'cyan'}/>
-
-         <UserBox position={[-4, -1.3, 0]} username={"hamza"} boxcolor={'yellow'}/>
-         <UserBox position={[-4, -2.3, 0]} username={"hamza"} boxcolor={'blue'}/>
-
-
-         <UserBox position={[-4, 1.3, 0]} username={"hamza"} boxcolor={'red'}/>
-         <UserBox position={[-4, 2.3, 0]} username={"hamza"} boxcolor={'magenta'}/>
-
-
-         <UserBox position={[2.5, -1.8, 0]} username={"hamza"} boxcolor={'chartreuse'}/>
-         <UserBox position={[2.5, 1.8, 0]} username={"hamza"} boxcolor={'blue'}/>
-
-
-         <UserBox position={[-2.5, -1.8, 0]} username={"hamza"} boxcolor={'cyan'}/>
-         <UserBox position={[-2.5, 1.8, 0]} username={"hamza"} boxcolor={'green'}/>
-
-         <UserBox position={[1.6,0,0]} username={"hamza"} boxcolor={'blue'}/>
-         <UserBox position={[-1.6, 0, 0]} username={"hamza"} boxcolor={'chartreuse'}/>
-         
-         <UserBox position={[0, 1, 0]} username={"hamza"} boxcolor={'white'}/> */}
       </>
 
    )
@@ -121,111 +80,64 @@ const DisplayUser = () => {
 
 const Tour = () => {
    const texture = useLoader(TextureLoader, "copper.jpg");
-   const { socket, user } = useAuth();
-   const location = useLocation()
-   const navigatedTour = location.state.tournament
-   console.log("the navigatedTour is :", navigatedTour)
+   const { socket, user, tokens, username } = useAuth();
+   const [tournament, setTournament] = useState(null)
+   const [invite, setInvite] = useState(false)
 
-
-   function send_tour_invite() {
-      if (socket) {
-         const message = JSON.stringify({
-            "type": 'tour_invite',
-            "tour_id" : 1,
-            "receiver" : JSON.stringify({
-               "id": 2,
-               "username":"hossin" 
-            })
-         })
-         socket.send(message);
-      }
+   function render_invite() {
+      setInvite(true)
    }
+   // const location = useLocation()
+   // const navigatedTour = location.state.tournament
+   // console.log("the navigatedTour is :", navigatedTour)
+   useEffect(() => {
+      const fetch_matches = async () => {
+         const response = await fetch('http://localhost:8000/tournament/10', {
+            headers: { Authorization: "JWT " + tokens.access }
+         })
+         const data = await response.json()
+         console.log("data matches : ", data)
+         setTournament(data)
+      }
+      fetch_matches()
+   }, [])
 
    return (
       <div className="h-[100%] w-[100%] flex flex-col relative gap-5">
-         <div className=" h-[70%] relative xsm:w-[99%] md:w-[70%] md:h-[70%] max-w-[1300px] rounded-[20px] flex justify-center items-center text-white flex-col bg-secondaryColor border-[2px] border-forthColor">
-            <Canvas >
+         <div className=" h-[70%] relative xsm:w-[99%] md:w-[70%]  max-w-[1300px] md:h-[70%] rounded-[20px] flex justify-center items-center text-white flex-col bg-secondaryColor border-[2px] border-forthColor">
+            {tournament && <Canvas >
                <ambientLight intensity={0.7} rotation={[Math.PI / 2, 0, 0]} />
                <pointLight position={[10, 10, 10]} rotation={[0, Math.PI / 2, 0]} />
                <OrbitControls />
                <Stage adjustCamera={true} intensity={1}>
-                  <DisplayUser />
+                  <DisplayUser tournament={tournament} />
                </Stage>
-            </Canvas>
+            </Canvas>}
          </div>
-         <div className="h-[calc(100%-70%)] xsm:w-[99%] md:w-[70%] gap-2 rounded-[20px] flex-col flex text-white   bg-secondaryColor border-[2px] border-forthColor justify-between font-inter">
+         <div className="h-[calc(100%-80%)] xsm:w-[99%] md:w-[70%]  max-w-[1300px] gap-2 rounded-[20px] flex-col flex text-white   bg-secondaryColor border-[2px] border-forthColor justify-between font-inter">
             <div className="flex justify-center w-[100%]  min-w-[150px] ">
                <p className="font-Valorax">PLAYER IN TOURNAMENT</p>
             </div>
             <div className=" flex justify-end gap-4 mx-10">
-               <button className="border-[2px] border-forthColor rounded-[5px]">START TOURNAMENT</button>
+               {tournament && tournament.creator.username == username && <button className="border-[2px] border-forthColor rounded-[5px]">START TOURNAMENT</button>}
                <GiExitDoor fontSize={30} className="" />
             </div>
-            <div className="flex  w-[100%] h-[70%]  flex-wrap justify-center">
-               <div className="flex items-center justify-center w-[23%] h-[33%] gap-3 border-[2px] border-forthColor m-1  min-w-[150px] rounded-[20px]">
-                  <button className="relative w-1/2 flex align-center justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-800 hover:bg-indigo-600 focus:outline-none" onClick={send_tour_invite}>Invite</button>
-               </div>
-               {/* <div className="flex items-center  w-[23%] h-[33%] gap-3 border-[2px] border-forthColor m-1  min-w-[150px] rounded-[20px]">
-                  <img src="./silver.jpg" className="rounded-full w-[52px] h-[52px]" />
-                  <div className="flex flex-col">
-                     <p>Hamza Younsi</p>
-                     <p className="text-[10px] font-inter">RANK 2</p>
+            <div className="flex  w-[100%] h-[70%]  flex-wrap ">
+               {tournament && tournament.players.map((item) =>
+                  <div className="flex items-center  w-[23%] h-[33%] gap-3 border-[2px] border-forthColor m-1  min-w-[150px] rounded-[20px]">
+                     <img src="./silver.jpg" className="rounded-full w-[52px] h-[52px]" />
+                     <div className="flex flex-col">
+                        <p>{item.username}</p>
+                        <p className="text-[10px] font-inter">RANK {item.rank}</p>
+                     </div>
                   </div>
-               </div>
-               <div className="flex items-center  w-[23%] h-[33%] gap-3 border-[2px] border-forthColor m-1  min-w-[150px] rounded-[20px]">
-                  <img src="./silver.jpg" className="rounded-full w-[52px] h-[52px]" />
-                  <div className="flex flex-col">
-                     <p>Hamza Younsi</p>
-                     <p className="text-[10px] font-inter">RANK 2</p>
-                  </div>
-               </div>
-               <div className="flex items-center  w-[23%] h-[33%] gap-3 border-[2px] border-forthColor m-1  min-w-[150px] rounded-[20px]">
-                  <img src="./silver.jpg" className="rounded-full w-[52px] h-[52px]" />
-                  <div className="flex flex-col">
-                     <p>Hamza Younsi</p>
-                     <p className="text-[10px] font-inter">RANK 2</p>
-                  </div>
-               </div>
-               <div className="flex items-center  w-[23%] h-[33%] gap-3 border-[2px] border-forthColor m-1  min-w-[150px] rounded-[20px]">
-                  <img src="./silver.jpg" className="rounded-full w-[52px] h-[52px]" />
-                  <div className="flex flex-col">
-                     <p>Hamza Younsi</p>
-                     <p className="text-[10px] font-inter">RANK 2</p>
-                  </div>
-               </div>
-               <div className="flex items-center  w-[23%] h-[33%] gap-3 border-[2px] border-forthColor m-1  min-w-[150px] rounded-[20px]">
-                  <img src="./silver.jpg" className="rounded-full w-[52px] h-[52px]" />
-                  <div className="flex flex-col">
-                     <p>Hamza Younsi</p>
-                     <p className="text-[10px] font-inter">RANK 2</p>
-                  </div>
-               </div>
-               <div className="flex items-center  w-[23%] h-[33%] gap-3 border-[2px] border-forthColor m-1  min-w-[150px] rounded-[20px]">
-                  <img src="./silver.jpg" className="rounded-full w-[52px] h-[52px]" />
-                  <div className="flex flex-col">
-                     <p>Hamza Younsi</p>
-                     <p className="text-[10px] font-inter">RANK 2</p>
-                  </div>
-               </div>
-               <div className="flex items-center  w-[23%] h-[33%] gap-3 border-[2px] border-forthColor m-1  min-w-[150px] rounded-[20px]">
-                  <img src="./silver.jpg" className="rounded-full w-[52px] h-[52px]" />
-                  <div className="flex flex-col">
-                     <p>Hamza Younsi</p>
-                     <p className="text-[10px] font-inter">RANK 2</p>
-                  </div>
-               </div>
-               <div className="flex items-center  w-[23%] h-[33%] gap-3 border-[2px] border-forthColor m-1  min-w-[150px] rounded-[20px]">
-                  <img src="./silver.jpg" className="rounded-full w-[52px] h-[52px]" />
-                  <div className="flex flex-col">
-                     <p>Hamza Younsi</p>
-                     <p className="text-[10px] font-inter">RANK 2</p>
-                  </div>
-               </div> */}
+               )}
+               {tournament && !tournament.is_full && <div className="flex items-center justify-center w-[23%] h-[33%] gap-3 border-[2px] border-forthColor m-1  min-w-[150px] rounded-[20px]">
+                  <IoMdPersonAdd size={30} onClick={render_invite}/>
+               </div>}
             </div>
-
-
-
          </div>
+         {invite && <InviteModal setInvite={setInvite} />}
       </div>
    );
 };
